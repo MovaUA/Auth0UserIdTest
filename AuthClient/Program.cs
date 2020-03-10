7dddc105ffa897fcbc4386a828b2d8c389d85c71
Valeriy Molchanov <valeriy.molchanov.77@gmail.com>
2020-03-10 14:04:49 +0200

    Add project files.

---
+++ AuthClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
